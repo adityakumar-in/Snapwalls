9721e9252c116dcb857ef22b36b65d6306239cd6
Aditya Kumar <adityaakumar.in@gmail.com>
2024-09-20 21:29:59 +0530

    Set Website color theme

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+import './globals.css';
+
 export const metadata = {
   title: "Snapwalls - Ultra HD Wallpapers",
   description: "We'll provide you with the best wallpapers for your different devices.",
